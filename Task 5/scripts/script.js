51c4ca06f03d870e5a9a9703aded2abb455a32f6
VladimirRogulev <vladimir.rogulev@gmail.com>
2020-01-17 15:32:02 +0300

    delete alert

--- Task 5/scripts/script.js
+++ Task 5/scripts/script.js
@@ -2,6 +2,6 @@ const firstNumber = +prompt('Введите 1-ое число:');
 const secondNumber = +prompt('Введите 2-ое число:');
 
 alert (isNaN(firstNumber) || isNaN(secondNumber) ? 'Вы указали значение неподходящего типа' :
-alert (firstNumber > secondNumber ? 
+(firstNumber > secondNumber ? 
     `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}` : 
     `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`));
